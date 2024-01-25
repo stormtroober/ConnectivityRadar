1cc7f3a20bf072fcb916b5e0e113f704fe55809e
aleshark87 <trofui@gmail.com>
2024-01-25 01:15:27 +0100

    Server bluetooth

---
+++ app/src/main/java/com/ds/connectivityradar/bluetooth/ServerThread.kt
@@ -0,0 +1,75 @@
+package com.ds.connectivityradar.bluetooth
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothServerSocket
+import android.bluetooth.BluetoothSocket
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
+import java.util.UUID
+import android.util.Log
+import androidx.core.content.ContextCompat
+
+
+import com.ds.connectivityradar.MainActivity
+import com.ds.connectivityradar.permissions.PermissionManager
+import java.io.IOException
+
+class ServerThread(private val btAdapter: BluetoothAdapter, private val activity: MainActivity) : Thread() {
+
+    private val permissionManager = PermissionManager(activity)
+    private var serverSocket: BluetoothServerSocket? = null
+
+    private fun initialiseSocket(): BluetoothServerSocket? {
+        val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
+            try {
+                if(permissionManager.isPermissionGranted(Manifest.permission.BLUETOOTH_CONNECT)) {
+                    btAdapter.listenUsingRfcommWithServiceRecord("ConnectivityRadar", UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"))
+                } else {
+                    null
+                }
+            } catch (e: SecurityException) {
+                Log.e("ServerThread", "SecurityException while creating BluetoothServerSocket", e)
+                null
+            }
+        }
+        val socket = mmServerSocket
+        return socket
+    }
+
+    override fun run() {
+        serverSocket = initialiseSocket()
+        Log.i("ServerThread", "Server bluetooth")
+        // Keep listening until exception occurs or a socket is returned.
+        var shouldLoop = true
+        while (shouldLoop) {
+            val socket: BluetoothSocket? = try {
+                serverSocket?.accept()
+            } catch (e: IOException) {
+                Log.e(TAG, "Socket's accept() method failed", e)
+                shouldLoop = false
+                null
+            }
+            socket?.also {
+                manageMyConnectedSocket(it)
+                serverSocket?.close()
+                shouldLoop = false
+            }
+        }
+    }
+
+    private fun manageMyConnectedSocket(socket: BluetoothSocket) {
+        Log.i("ServerThread", "Message arrived at socket")
+    }
+
+    // Closes the connect socket and causes the thread to finish.
+    fun cancel() {
+        try {
+            serverSocket?.close()
+        } catch (e: IOException) {
+            Log.e(TAG, "Could not close the connect socket", e)
+        }
+    }
+}
